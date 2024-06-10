8c7134c9cb4c1a05a3cc7d34a741b2f0632e53f2
catalinaHea <alisagosens@gmail.com>
2024-06-10 21:37:07 +0300

    aa
    aaa

---
+++ src/validators/option-validator.ts
@@ -0,0 +1,5 @@
+export const COLORS = [
+    {label: "Black", value: "black", tw:"zinc-900"},
+    {label: "Blue", value: "blue", tw:"blue-900"},
+    {label: "Rose", value: "rose", tw:"rose-900"},
+] as const
